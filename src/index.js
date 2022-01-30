394019fc7a370a861fb5668c8deb040367414e41
quentin <quentinlao@outlook.com>
2022-01-30 15:22:02 +0100

    [RT] initialization project react hot reload and minimum config jsx

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+const App = () => {
+ return <h1>React App template</h1>;
+ }
+ReactDOM.render(<App />, document.getElementById('app'));
