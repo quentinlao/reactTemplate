12da4693ca9f6094d280d084541e60dc1abd76b8
quentin <quentinlao@outlook.com>
2022-02-20 21:36:04 +0100

    doc update jest configuration explanation

--- .jest/fileMock.js
+++ .jest/fileMock.js
@@ -1,1 +1,6 @@
-module.exports = 'test-file-stub';
+/**
+ * testEnvironment default : node https://stackoverflow.com/questions/69227566/consider-using-the-jsdom-test-environment
+ * Jest encountered an unexpected token
+ * A map from regular expressions to module names or to arrays of module names for Files
+ */
+module.exports = {};
